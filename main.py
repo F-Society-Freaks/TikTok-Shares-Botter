1fcdb406a7073a42a85fef8c5b398a8227f744b8
zoony <58914699+zoony1337@users.noreply.github.com>
2020-08-01 01:39:56 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,107 @@
+import os
+import random
+import requests
+import threading
+from time import strftime, gmtime, time, sleep
+
+
+class TikTok:
+    def __init__(self):
+        self.added = 0
+        self.lock = threading.Lock()
+
+        try:
+            self.amount = int(input('> Amount: '))
+        except ValueError:
+            print('\nInteger expected.')
+            os.system('title [TikTok Shares Botter] - Restart required')
+            os.system('pause >NUL')
+            os._exit(0)
+
+        try:
+            self.video_id = input('> TikTok URL: ').split('/')[5]
+        except IndexError:
+            print(
+                '\nInvalid TikTok URL format.\nFormat expected: https://www.tiktok.com/@username/vi'
+                'deo/1234567891234567891'
+            )
+            os.system('title [TikTok Shares Botter] - Restart required')
+            os.system('pause >NUL')
+            os._exit(0)
+        else:
+            print()
+
+    def status(self, code, intention):
+        if code == 200:
+            self.added += 1
+        else:
+            self.lock.acquire()
+            print(f'Error: {intention} | Status Code: {code}')
+            self.lock.release()
+
+    def update_title(self):
+        # Avoid ZeroDivisionError
+        while self.added == 0:
+            sleep(0.2)
+        while self.added < self.amount:
+            # Elapsed Time / Added * Remaining
+            time_remaining = strftime(
+                '%H:%M:%S', gmtime(
+                    (time() - self.start_time) / self.added * (self.amount - self.added)
+                )
+            )
+            os.system(
+                f'title [TikTok Shares Botter] - Added: {self.added}/{self.amount} '
+                f'({round(((self.added / self.amount) * 100), 3)}%) ^| Active Threads: '
+                f'{threading.active_count()} ^| Time Remaining: {time_remaining}'
+            )
+            sleep(0.2)
+        os.system(
+            f'title [TikTok Shares Botter] - Added: {self.added}/{self.amount} '
+            f'({round(((self.added / self.amount) * 100), 3)}%) ^| Active Threads: '
+            f'{threading.active_count()} ^| Time Remaining: 00:00:00'
+        )
+
+    def bot(self):
+        action_time = round(time())
+        install_id = ''.join(random.choice('0123456789') for _ in range(19))
+
+        data = (
+            f'action_time={action_time}&item_id={self.video_id}&item_type=1&share_delta=1&stats_cha'
+            'nnel=copy'
+        )
+
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'x-common-params-v2': 'version_code=16.6.5&app_name=musical_ly&channel=App%20Store&devi'
+                                  f'ce_id={install_id}&aid=1233&os_version=13.5.1&device_platform=i'
+                                  'phone&device_type=iPhone10,5'
+        }
+
+        try:
+            response = requests.post(
+                'https://api16-core-c-useast1a.tiktokv.com/aweme/v1/aweme/stats/?ac=WIFI&op_region='
+                'SE&app_skin=white&', data=data, headers=headers
+            )
+        except Exception as e:
+            print(f'Error: {e}')
+        else:
+            if 'Service Unavailable' not in response.text:
+                self.status(response.status_code, response.text)
+
+    def multi_threading(self):
+        self.start_time = time()
+        threading.Thread(target=self.update_title).start()
+
+        for _ in range(self.amount):
+            threading.Thread(target=self.bot).start()
+
+        os.system('pause >NUL')
+        os.system('title [TikTok Shares Botter] - Exiting...')
+        sleep(3)
+
+
+if __name__ == '__main__':
+    os.system('cls && title [TikTok Shares Botter]')
+    main = TikTok()
+    main.multi_threading()
